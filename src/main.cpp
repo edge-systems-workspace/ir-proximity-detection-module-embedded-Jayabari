8f3b78e73b36304792e6dca44a7ab2945174434b
Jaya <jayabari849@gmail.com>
2026-02-20 09:46:16 +0530

    implement obstacle detection logic and configure IR sensor

--- src/main.cpp
+++ src/main.cpp
@@ -2,43 +2,27 @@
 /**
  * @file main.cpp
  * @brief Embedded Obstacle Detection System using IR Sensor
- * @author YOUR_NAME
- * @date YYYY-MM-DD
+ * @author Jaya
+ * @date 2026-2-6
  *
  * @details
  * Reads digital input from IR sensor
  * and detects obstacle presence.
  */
 
- // TODO 1:
- // Define IR sensor digital pin (Use pin 2)
-
- // TODO 2:
- // Create variable to store sensor state
+#define irPin 2
 
 void setup() {
-
-    // TODO 3:
-    // Initialize Serial communication (9600 baud rate)
-
-    // TODO 4:
-    // Configure IR pin as INPUT
-
-    // TODO 5:
-    // Print system initialization message
+    pinMode(irPin, INPUT);
+    //Serial.begin(9600);
 }
 
 void loop() {
-
-    // TODO 6:
-    // Read digital value from IR sensor
-
-    // TODO 7:
-    // If obstacle detected
-    //     Print "Obstacle Detected"
-    // Else
-    //     Print "No Obstacle"
-
-    // TODO 8:
-    // Add small delay (200–500ms)
+    int irValue = digitalRead(irPin);
+    if (irValue == HIGH) {
+        Serial.println("IR SENSOR: NO Obstacles ");
+    }else {
+        Serial.println("IR SENSOR: Obstacles detected");
+    }
+    delay(1000);
 }
